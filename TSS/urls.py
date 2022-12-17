cb87b67d662a5a8f9356439d958ad15679d62fb0
Yarias2106 <zekrom2110@gmail.com>
2022-12-16 21:28:51 -0400

    viesw.py urls

--- TSS/urls.py
+++ TSS/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from TSS.views import holaMund
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("holamundo/", holaMund)
 ]
---
+++ TSS/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def holaMund(request):
+
+   return render(request, "holamundo.html")
